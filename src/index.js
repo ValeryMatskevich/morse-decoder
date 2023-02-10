d53423b778a505f1139488b87e745a94c7f14bb0
Valery Matskevich <v.matskevich93@gmail.com>
2023-02-10 23:14:25 +0300

    feat:add a solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,47 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = []
+    let str = ''
+    let start = 0
+    let end = 10
+    while (result.length !== expr.length/10) {
+      result.push(expr.slice(start, end))
+      start += 10
+      end+=10
+    }
+
+    for (let i = 0; i < result.length; i++) {
+      if (result[i] === '**********') {
+        str += ' '
+      } else {
+          let firstOccurrence = result[i].indexOf('1')
+          let numericСode = result[i].slice(firstOccurrence)
+          let numericСodeArray = []
+          let zero = 0
+          let one = 2
+
+          while (numericСodeArray.length < numericСode.length/2) {
+            numericСodeArray.push(numericСode.slice(zero, one))
+            zero += 2
+            one += 2
+          }
+
+          let dashPoint = ''
+
+          for (let i = 0; i < numericСodeArray.length; i++) {
+            if (numericСodeArray[i] === '10') {
+              dashPoint+='.'
+            } else {
+              dashPoint+='-'
+            }
+
+          }
+          str += MORSE_TABLE[dashPoint]
+      }
+    }
+    return str
+
 }
 
 module.exports = {
